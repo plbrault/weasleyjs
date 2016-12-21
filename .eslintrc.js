8281886e65bec34b68d646fcc038490b62e1221f
Pier-Luc Brault <plbrault@novomnetworks.com>
2016-12-21 16:46:14 -0500

    Fix bugs

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: 'airbnb',
+  plugins: [
+  ],
+  rules: {
+    'react/jsx-filename-extension': 'off',
+    'react/prefer-stateless-function': 'off',
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,41 +1,47 @@
-'use strict';
-
 export default class Weasley {
   constructor() {
     this.resolvers = {};
-    this.modules = {};
+    this.moduleProxies = {};
     this.container = {};
   }
 
-  register(key, resolver, doNotUseDefault) {
-    this.resolvers[key] = resolver;
-    this.modules[key] = null;
-    const parts = key.split('.');
-    let obj = this.container;
-    for (let i = 0; i < parts.length; i++) {
-      const part = parts[i];
-      if (i < parts.length - 1) {
-        if (!obj[part]) {
-          obj[part] = {};
-        }
-        obj = obj[part];
-      } else {
-        if (!obj.hasOwnProperty(part)) {
-          Object.defineProperty(obj, part, {
-            get: () => {
-              return this.modules[key] || 
-                (() => {
-                  let module = this.resolvers[key]();
-                  if (module.default && !doNotUseDefault) {
-                    module = module.default;
-                  }
-                  this.modules[key] = module;
-                  return module;
-                })();
-            }
-          });
-        }
+  updateModuleProxy(key) {
+    const { resolver, doNotUseDefault } = this.resolvers[key];
+    let module = resolver();
+    if (module.default && !doNotUseDefault) {
+      module = module.default;
+    }
+
+    const moduleProxy = this.moduleProxies[key] || {};
+    for (let attr in moduleProxy) {
+      if (moduleProxy.hasOwnProperty(attr)) {
+        delete moduleProxy[attr];
       }
+    }
+    Object.assign(moduleProxy, module);
+
+    this.moduleProxies[key] = moduleProxy;
+    return moduleProxy;
+  }
+
+  register(key, resolver, doNotUseDefault) {
+    this.resolvers[key] = { resolver, doNotUseDefault };
+
+    if (this.moduleProxies[key]) {
+      this.updateModuleProxy(key);
+    } else {
+      const parts = key.split('.');
+      let container = this.container;
+
+      for (let i = 0; i < parts.length - 1; i += 1) {
+        const part = parts[i];
+        container[part] = {};
+        container = container[part];
+      }
+
+      Object.defineProperty(container, parts[parts.length - 1], {
+        get: () => this.moduleProxies[key] || this.updateModuleProxy(key),
+      });
     }
   }
 }
